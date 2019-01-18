e291bbbe843c628040b817c5d87975c63fa3f26b
Jones-2 <46811906+Jones-2@users.noreply.github.com>
2019-01-18 19:52:40 +0000

    Update PartC.java

--- javaProgram/src/PartC.java
+++ javaProgram/src/PartC.java
@@ -10,10 +10,11 @@ public class PartC {
         Scanner myScanner = new Scanner(System.in);
 
         out.print("Please input your sentence. ");
-
+// allows user to enter info
         String statement = myScanner.nextLine();
-        String reverse = "";
-        String words[] = statement.split(" ");
+        String reverse = "";//empty string to allow concatination
+        String words[] = statement.split(" ");//spliting the word
+        //the for look allows the reversal of each letter in the word
         for (String eachWord : words){
             for(int i=eachWord.length()-1; i>-1; i--){
                 reverse = reverse.concat("" + eachWord.charAt(i));
@@ -22,10 +23,17 @@ public class PartC {
         }
         return reverse;
     }
-
+    public static void findLength() {
+			//stores string data
+			String input="programming";
+			//finds the length of the string above
+			int strlength=input.length();
+			System.out.println("string length:"+ strlength);
+		}
+//the method below calculates the area of a circle and circumference of a circle
     private static void circumArea(double radius){
-        double circumference = 2*Math.PI*radius;
-        double area = Math.PI*radius*radius;
+        double circumference = 2*Math.PI*radius;//calculates the circumference of the circle
+        double area = Math.PI*radius*radius;//calculates the area of a circle
         System.out.println("Area: " + area + " units square\nCircumference: "+ circumference +" units\n");
     }
 
@@ -33,5 +41,6 @@ public class PartC {
         System.out.println(reverseString());
         System.out.println("\n ********************************************* \n");
         circumArea(7);
+        findLength();
     }
 }
